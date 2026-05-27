4a2341555fc1f9e19ed0392917829070846cc814
anl084 <anl084@ucsd.edu>
2026-05-26 17:06:11 -0700

    Part A done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -65,19 +65,28 @@ function initializeServiceWorker() {
  * @returns {Array<Object>} An array of recipes found in localStorage
  */
 async function getRecipes() {
-  // EXPOSE - START (All expose numbers start with A)
-  // A1. TODO - Check local storage to see if there are any recipes.
-  //            If there are recipes, return them.
-  /**************************/
-  // The rest of this method will be concerned with requesting the recipes
-  // from the network
-  // A2. TODO - Create an empty array to hold the recipes that you will fetch
-  // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
-  //            has a great article on them. A promise takes one parameter - A
-  //            function (we call these callback functions). That function will
-  //            take two parameters - resolve, and reject. These are functions
-  //            you can call to either resolve the Promise or Reject it.
-  /**************************/
+  const recipes = JSON.parse(localStorage.getItem("recipes")) || [];
+  if(recipes.length > 0){
+    return recipes;
+  }
+
+  // promise with parameter reject and rwsolve
+  const myPromise = new Promise(async (resolve, reject) => {
+    try{
+      for(const recipe of RECIPE_URLS){
+        const response = await fetch(recipe);
+        const json = await recipeFetched.json();
+        recipes.push(json);
+      }
+      saveRecipesToStorage(recipes);
+      resolve(recipes);
+    }
+    catch(error){
+      console.error(error);
+      reject(error);
+    }
+  });
+  return myPromise;
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
   /**************************/
